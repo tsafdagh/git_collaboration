a19d9aa2ad86833c7ad7f805011f335f76e3f3f9
kwaminelson <kwamitchamounelson@gmail.com>
2019-08-21 10:00:54 +0200

    modivication du fichier commun MainActivity

--- app/src/main/java/com/tsafack/android_git_collaboration/MainActivity.kt
+++ app/src/main/java/com/tsafack/android_git_collaboration/MainActivity.kt
@@ -2,11 +2,13 @@ package com.tsafack.android_git_collaboration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"je suis entrain de creer un conflit",Toast.LENGTH_LONG).show()
     }
 }
